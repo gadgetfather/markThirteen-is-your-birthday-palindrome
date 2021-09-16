2d7ef4161be23801c598ccc4c5266735834bc859
gadgetfather <kamblea635@gmail.com>
2021-09-16 11:11:10 +0530

    Update app.js
    converToDate function added

--- app.js
+++ app.js
@@ -14,5 +14,35 @@ function isPalindrome(str)
 
 }
 
-console.log(isPalindrome("hello"))
-console.log(isPalindrome("lol"))
+
+function convertDatetoStr(date){
+    var dateStr = {day:'', month:"",year:''}
+
+    if(date.day<10){
+        dateStr.day = '0'+ date.day
+    }
+    else{
+        dateStr.day = date.day.toString();
+    }
+    if(date.month<10){
+        dateStr.month = '0'+ date.month
+    }
+    else{
+        dateStr.month = date.month.toString();
+    }
+    
+    
+    dateStr.year = date.year.toString();
+
+    return dateStr;
+    
+}
+
+var date = {
+    day:2,
+    month:2,
+    year:2020
+    
+}
+
+console.log(convertDatetoStr(date))
