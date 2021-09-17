8a1a0c3d505c6cea8c399ea660b6a7d39bb41cc1
gadgetfather <kamblea635@gmail.com>
2021-09-17 10:46:43 +0530

    Update app.js
    all date format function added

--- app.js
+++ app.js
@@ -38,11 +38,22 @@ function convertDatetoStr(date){
     
 }
 
-var date = {
-    day:2,
-    month:2,
-    year:2020
-    
+function dateAllFormat(){
+    var dateStr = convertDatetoStr(date);
+
+    var ddmmyyyy = dateStr.day + dateStr.month+ dateStr.year
+    var mmddyyyy = dateStr.month+ dateStr.day + dateStr.year
+    var yyyymmdd = dateStr.year + dateStr.month + dateStr.day
+    var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2)
+    var mmddyy = dateStr.month+ dateStr.day + dateStr.year.slice(-2)
+    var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day
+
+    return [ddmmyyyy,mmddyyyy,yyyymmdd,ddmmyy,mmddyy,yymmdd]
 }
 
-console.log(convertDatetoStr(date))
+var date = {
+    day: 2,
+    month:12,
+    year:2202
+}
+console.log(dateAllFormat(date))
